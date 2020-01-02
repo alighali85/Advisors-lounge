cccbbb50e4d885e27f7818bd5fdbd7cf00bfbdf6
Ali Ghali <alifriends@live.com>
2020-01-02 02:42:34 +0100

    feat(containers): add app navbar

---
+++ src/containers/appNavbar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class AppNavbar extends React.Component {
+    render() {
+        return <>
+            <div className="app-navbar">
+            </div>
+        </>
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import {getData} from './helpers/mockedData'
+import AppNavbar from './containers/appNavbar'
 
 class App extends React.Component {
     state = {
