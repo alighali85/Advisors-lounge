469c6e2346a9f6f8a36dcdd892dbf696c7f11f21
Ali Ghali <alifriends@live.com>
2020-01-05 15:58:47 +0100

    feat(cards): use cards to show advisors

---
+++ src/containers/advisor/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import './advisor.scss'
+import Rating from 'react-rating'
+
+interface AdvisorProps {
+    id: number,
+    name?: string;
+    title?: string;
+    bio?: string;
+    rating: number
+}
+
+export default class Advisor extends React.Component<AdvisorProps> {
+    constructor(props: any) {
+        super(props)
+    }
+
+    render() {
+        return <>
+            <div>
+                <Card style={{ width: '18rem' }}>
+                    {/* <Card.Img variant="top" src={this.props.image} /> */}
+                    <Card.Body>
+                        <Card.Title>{this.props.name}</Card.Title>
+                        <Card.Text>
+                            <i>{this.props.bio}</i>
+                            <span>{this.props.title}</span>
+                            <br />
+                            <Rating placeholderRating={this.props.rating} />
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        </>
+    }
+}
